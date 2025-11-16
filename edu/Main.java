17787c5bf0a78000703d246d7338d701acb1e018
Egor Gutsalov <egor.gutsalov@gmail.com>
2025-11-16 21:49:19 +0300

    final classes

--- edu/Main.java
+++ edu/Main.java
@@ -1,6 +1,53 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to Java programming!");
+        practice1();
+        practice2();
+    }
+
+    public static void practice1() {
+        /*
+           В этом примере попытка переопределить final-метод showMessage()
+           в классе B приведёт к ошибке компиляции.
+        */
+        A a = new A();
+        a.showMessage();
+    }
+
+    public static void practice2() {
+        /*
+           В этом примере попытка унаследоваться от FinalClass
+           приведёт к ошибке компиляции.
+        */
+        FinalClass f = new FinalClass();
+        f.print();
     }
 }
+
+
+class A {
+
+    public final void showMessage() {
+        System.out.println("Final method in A");
+    }
+}
+
+// Ошибка: нельзя переопределить final-метод
+// class B extends A {
+//     @Override
+//     public void showMessage() {   // ошибка компиляции
+//         System.out.println("Try to override");
+//     }
+// }
+
+
+
+final class FinalClass {
+
+    public void print() {
+        System.out.println("Final class method");
+    }
+}
+
+// Ошибка: final-класс нельзя наследовать
+// class Child extends FinalClass { }  // ошибка компиляции
