ef3c5c8a6ab1fd505ee200a221a1c6851ec26230
Egor Gutsalov <egor.gutsalov@gmail.com>
2025-11-16 19:37:20 +0300

    ternary operator

--- edu/Main.java
+++ edu/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to Java programming!");
+
+        int a = 2;
+        int i = (a == 1) ? 1 : (a == 2 ? 2 : 3);
+
+        System.out.println(i);
     }
 }
